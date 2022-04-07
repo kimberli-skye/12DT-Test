b7cec671c76318045d3cb7e9269d3be257345a5b
Kimberli Jones <103225527+kimberli-skye@users.noreply.github.com>
2022-04-08 11:48:45 +1200

    Added print videogames function
    Added a function that loops through the dictionary and prints it out in a proper sentence

--- videogame_rater.py
+++ videogame_rater.py
@@ -6,5 +6,13 @@
 # 08/04
 
 
+def print_dictionary(dictionary):
+    """ Accepts a Dictionary and loops through it and prints the keys and values (games and rating)"""
+    for game, rating in dictionary.items():
+        print("{} has a {} out of 5 stars rating".format(game, rating))
+
+
 if __name__ == "__main__":
     videogames = {"Minecraft":5, "Call of Duty":1, "Angry Birds":4, "Splatoon 2":5, "Animal Crossing":4,}
+
+    print_dictionary(videogames)
