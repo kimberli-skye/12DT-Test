261ff12b2a8c981731ae1b6b2c82f73f9d084bec
Kimberli Jones <103225527+kimberli-skye@users.noreply.github.com>
2022-04-08 11:44:37 +1200

    main routine and intitial collection
    Created a main routine. Added a dictionary with some games and their rating out of five stars.

---
+++ videogame_rater.py
@@ -0,0 +1,10 @@
+#
+# videogame_rater.py
+# Rates video games
+#
+# KJ
+# 08/04
+
+
+if __name__ == "__main__":
+    videogames = {"Minecraft":5, "Call of Duty":1, "Angry Birds":4, "Splatoon 2":5, "Animal Crossing":4,}
