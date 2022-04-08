4444e63377a23f689ed236c738a59e662a77717d
Kimberli Jones <103225527+kimberli-skye@users.noreply.github.com>
2022-04-08 12:18:41 +1200

    3d dictionary
    Changed the dictionary to a 3d dictionary, updated the print function to print it out properly

--- videogame_rater.py
+++ videogame_rater.py
@@ -8,11 +8,19 @@
 
 def print_dictionary(dictionary):
     """ Accepts a Dictionary and loops through it and prints the keys and values (games and rating)"""
-    for game, rating in dictionary.items():
-        print("{} has a {} out of 5 stars rating".format(game, rating))
+    for ID, game in dictionary.items():
+        print("ID = {}, {} has a {} out of 5 stars rating".format(ID, game["name"], game["rating"]))
+
+    
+
 
 
 if __name__ == "__main__":
-    videogames = {"Minecraft":5, "Call of Duty":1, "Angry Birds":4, "Splatoon 2":5, "Animal Crossing":4,}
+    videogames = {1:{"name":"Minecraft", "rating":5},
+                              2:{"name":"Call of Duty", "rating":1},
+                              3:{"name":"Angry Birds", "rating":4},
+                              4:{"name":"Splatoon 2", "rating":5},
+                              5:{"name":"Animal Crossing", "rating":4}
+                              }
 
     print_dictionary(videogames)
